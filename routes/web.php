0366c75fe35a631b88df9bf59f27ad6f23795009
vika <vika3834@gmail.com>
2022-01-22 15:14:46 +0300

    login and auth

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/article', [App\Http\Controllers\ArticleController::class, 'show']);
 Route::get('/create', [\App\Http\Controllers\ArticleController::class, 'showCreate']);
 Route::post('/create', [\App\Http\Controllers\ArticleController::class, 'store'])->name('create');
 Route::get('/created', [\App\Http\Controllers\ArticleController::class, 'showArticle']);
+Route::get('/auth', [\App\Http\Controllers\AuthController::class, 'showAuth']);
+Route::post('/auth', [\App\Http\Controllers\AuthController::class, 'checkUser'])->name('auth');
+Route::get('/reg', [\App\Http\Controllers\RegController::class, 'showReg']);
+Route::post('/reg', [\App\Http\Controllers\RegController::class, 'addUser'])->name('reg');
